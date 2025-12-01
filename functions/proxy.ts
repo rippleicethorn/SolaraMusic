ccd728974d191ded3ff3f472c33132526c077d16
rippleicethorn <80150029+rippleicethorn@users.noreply.github.com>
2025-12-01 20:22:33 +0800

    Update proxy.ts

--- functions/proxy.ts
+++ functions/proxy.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://g-hwrs3273.coding.net/api/";
+const API_BASE_URL = "http://api-music.1fooai.com/api/musicApi.php";
 const KUWO_HOST_PATTERN = /(^|\.)kuwo\.cn$/i;
 const SAFE_RESPONSE_HEADERS = ["content-type", "cache-control", "accept-ranges", "content-length", "content-range", "etag", "last-modified", "expires"];
 
